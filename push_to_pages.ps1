5efe2b3267d634978ad96988e4216a6194b3608d
idems-bot1 <git.bot@idems.international>
2020-07-23 18:33:49 +0100

    Remove virtual maths camp folder added by mistake

--- push_to_pages.ps1
+++ push_to_pages.ps1
@@ -1,16 +1,16 @@
 if ( -not (Test-Path -Path './virtual-maths-camp-gh-pages' -PathType Container) ) {
     git clone https://github.com/IDEMSInternational/virtual-maths-camp-gh-pages.git
 }
-
 cd ./virtual-maths-camp-gh-pages
+git pull
 if (Test-Path -Path './docs' -PathType Container) {
     Remove-Item -Recurse -Force docs
 }
 mkdir docs
 Copy-Item -Path "../dist/virtual-maths-camp-ui/*" -Destination "./docs" -Recurse
-cd ../
 git config user.name idems-bot1
 git config user.email git.bot@idems.international
 git add .
 git commit -m "Update with latest angular build"
-git status
+git push
+cd ../
